1ceff03d645c1b86f71df228e75502ef5203fe0e
Luciano Moreyra <luciano.moreyra774@outlook.com.ar>
2025-08-26 02:41:57 -0300

    nuevo agosto

--- main2.js
+++ main2.js
@@ -1,4 +1,9 @@
 const mensaje = document.getElementById("mensaje");
 
 let descuentoInicial;
+
 //una cierta logica que puede modificar el descuentoInicial
+
+const descuentoPredeterminado = 10;
+let descuentoActual = descuentoInicial ?? descuentoPredeterminado
+mensaje.textContent = `El descuento es del ${descuentoActual}%`
