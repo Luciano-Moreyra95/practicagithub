18f9cc0c3192832720c829eb418f50156e072c9d
Luciano Moreyra <luciano.moreyra774@outlook.com.ar>
2025-09-01 00:53:06 -0300

    ejercisios

--- script.js
+++ script.js
@@ -60,3 +60,75 @@ console.log(index);
 nombres.splice(index, 1);
 
 console.log(nombres);
+
+const auto = {
+    puertas: 3,
+    ruedas: 4,
+    color: "rojo",
+    modelo: "gol"
+}
+
+console.log(auto);
+
+console.log(auto.modelo);
+
+const autos = [
+    {
+        marca: "Volkswagen",
+        modelo: "Gol",
+        año: 2015,
+        puertas: 3,
+        color: "rojo",
+        transmision: "manual",
+        combustible: "nafta",
+        kilometraje: 85000,
+        precioUSD: 7500
+    },
+    {
+        marca: "Toyota",
+        modelo: "Corolla",
+        año: 2021,
+        puertas: 5,
+        color: "blanco",
+        transmision: "automática",
+        combustible: "híbrido",
+        kilometraje: 15000,
+        precioUSD: 23000
+    },
+    {
+        marca: "Ford",
+        modelo: "Mustang",
+        año: 2019,
+        puertas: 3,
+        color: "azul",
+        transmision: "manual",
+        combustible: "nafta",
+        kilometraje: 32000,
+        precioUSD: 35000
+    },
+    {
+        marca: "Honda",
+        modelo: "Civic",
+        año: 2020,
+        puertas: 5,
+        color: "gris",
+        transmision: "automática",
+        combustible: "nafta",
+        kilometraje: 27000,
+        precioUSD: 21000
+    },
+    {
+        marca: "Porsche",
+        modelo: "911 Carrera",
+        año: 2022,
+        puertas: 2,
+        color: "negro",
+        transmision: "automática",
+        combustible: "nafta premium",
+        kilometraje: 8000,
+        precioUSD: 120000
+    }
+]
+
+console.log(autos[4].puertas);
+
